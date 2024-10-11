badd26852023765fd4a139a98850d08e78560ee1
armorbug <vvladi30@gmail.com>
2024-10-11 10:51:26 +0300

    Challenge Project Renamed Guided Project 1 to Guided Project

--- C# course projects/CsharpSimpleConsoleApps/TestProject2/obj/Debug/net8.0/TestProject2.AssemblyInfo.cs
+++ C# course projects/CsharpSimpleConsoleApps/TestProject2/obj/Debug/net8.0/TestProject2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d1419902bf7a080a1862fd647cd0484e2fd25e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93f8bc4e5976074ad0e4b747e429648dda8c0cdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
