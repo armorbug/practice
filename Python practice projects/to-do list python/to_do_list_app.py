7d780e2b65ac91e1f998880e34d8789b01fa79b9
armorbug <vvladi30@gmail.com>
2024-10-11 15:17:00 +0300

    Print statement and formatting

--- Python practice projects/to-do list python/to_do_list_app.py
+++ Python practice projects/to-do list python/to_do_list_app.py
@@ -1,13 +1,32 @@
-#To-Do List App (CLI-Based)
+# To-Do List App (CLI-Based)
+
 
 def add_task():
     return 0
 
+
 def view_tasks():
     return 0
 
+
 def mark_task_as_complete():
+
     return 0
+
 
 def delete_task():
     return 0
+
+
+tasks = []
+
+print(
+    '''Hi, I am Vladimir and this is my To-Do List App.
+What would you like to do today?
+    1. View tasks
+    2. Add task
+    3. Mark task as completed
+    4. Delete task
+    5. Exit the app'''
+)
+option = input("Please type the number of the option you want to choose: ")
